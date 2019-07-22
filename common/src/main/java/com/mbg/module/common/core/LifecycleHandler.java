297b865866291f062ae9fc56a3f5de70139885c9
zhaozhiyang <zhaozhiyang@blued.com>
2019-07-22 10:33:31 +0800

    创建一个支持库

---
+++ common/src/main/java/com/mbg/module/common/core/LifecycleHandler.java
@@ -0,0 +1,53 @@
+package com.mbg.module.common.core;
+
+import android.os.Looper;
+import android.os.Handler;
+
+
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.OnLifecycleEvent;
+
+
+public final class LifecycleHandler extends Handler implements LifecycleObserver {
+    private LifecycleOwner mLifecycleOwner;
+
+    public LifecycleHandler(final LifecycleOwner lifecycleOwner) {
+        this.mLifecycleOwner = lifecycleOwner;
+        addObserver();
+    }
+
+    public LifecycleHandler(final Callback callback, final LifecycleOwner lifecycleOwner) {
+        super(callback);
+        this.mLifecycleOwner = lifecycleOwner;
+        addObserver();
+    }
+
+    public LifecycleHandler(final Looper looper, final LifecycleOwner lifecycleOwner) {
+        super(looper);
+        this.mLifecycleOwner = lifecycleOwner;
+        addObserver();
+    }
+
+    public LifecycleHandler(final Looper looper, final Callback callback, final LifecycleOwner lifecycleOwner) {
+        super(looper, callback);
+        this.mLifecycleOwner = lifecycleOwner;
+        addObserver();
+    }
+
+    private void addObserver() {
+        if (mLifecycleOwner!=null) {
+            mLifecycleOwner.getLifecycle().addObserver(this);
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private void onDestroy() {
+        removeCallbacksAndMessages(null);
+        if (mLifecycleOwner!=null) {
+            mLifecycleOwner.getLifecycle().removeObserver(this);
+        }
+    }
+
+}
---
+++ common/src/main/java/com/mbg/module/common/util/AppUtils.java
@@ -0,0 +1,98 @@
+package com.mbg.module.common.util;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class AppUtils {
+    private AppUtils(){}
+
+    private static Application sApplication;
+
+    /**
+     * 设置全局 Application
+     */
+    public static void setupApplication(@NonNull Application application) {
+        sApplication = application;
+    }
+
+    /**
+     * 获取全局 Application
+     */
+    public static Application getApplication() {
+        //如果没有设置则需要通过反射的方式来获取
+        if (sApplication==null){
+            try {
+                Class<?> activityThread = Class.forName("android.app.ActivityThread");
+                Method m_currentActivityThread = activityThread.getDeclaredMethod("currentActivityThread");
+                Field f_mInitialApplication = activityThread.getDeclaredField("mInitialApplication");
+                f_mInitialApplication.setAccessible(true);
+                Object current = m_currentActivityThread.invoke(null);
+                Object app = f_mInitialApplication.get(current);
+                sApplication = (Application) app;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sApplication;
+    }
+
+
+
+    /**
+     * 获取 App 的版本名
+     *
+     * @return app的版本名 默认为""
+     */
+    public static String getVersionName() {
+        Context context = getApplication();
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 获取 App 的版本号
+     *
+     * @return 获取版本号 默认为0
+     */
+    public static int getVersionCode() {
+        Context context = getApplication();
+        PackageManager pm = context.getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * @return App 是否可以 debug
+     */
+    public static boolean isAppDebugable() {
+        try {
+            ApplicationInfo info = getApplication().getApplicationInfo();
+            return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
