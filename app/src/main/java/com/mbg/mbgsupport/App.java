8d9f7c3e84bd173d06c9c91eb70844aacce66382
zhaozhiyang <zhaozhiyang@blued.com>
2019-07-29 18:54:04 +0800

    添加测试代码

--- app/src/main/java/com/mbg/mbgsupport/App.java
+++ app/src/main/java/com/mbg/mbgsupport/App.java
@@ -4,6 +4,11 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.mbg.module.ui.image.cache.common.Md5FileNameGenerator;
+import com.mbg.module.ui.image.cache.common.QueueProcessingType;
+import com.mbg.module.ui.image.cache.engine.imageloader.ImageLoader;
+import com.mbg.module.ui.image.cache.engine.imageloader.ImageLoaderConfiguration;
+
 
 public class App extends Application {
 
@@ -18,9 +23,26 @@ public class App extends Application {
         super.onCreate();
 
         //
-
+        initImageLoader(this);
 
     }
 
+
+    public static void initImageLoader(Context context) {
+        // This configuration tuning is custom. You can tune every option, you may tune some of them,
+        // or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        // method.
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+        config.writeDebugLogs(); // Remove for release app
+
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
+    }
 
 }
--- app/src/main/java/com/mbg/mbgsupport/fragment/DragFragment.java
+++ app/src/main/java/com/mbg/mbgsupport/fragment/DragFragment.java
@@ -7,6 +7,7 @@ import com.mbg.mbgsupport.R;
 import com.mbg.module.common.util.ClickUtils;
 import com.mbg.module.ui.activity.TerminalActivity;
 import com.mbg.module.ui.fragment.BaseFragment;
+import com.mbg.module.ui.image.view.RecyclerImageView;
 
 public class DragFragment extends BaseFragment implements View.OnClickListener{
 
@@ -24,6 +25,9 @@ public class DragFragment extends BaseFragment implements View.OnClickListener{
         findViewById(R.id.btn_test2).setOnClickListener(this);
         findViewById(R.id.btn_test3).setOnClickListener(this);
         findViewById(R.id.btn_test4).setOnClickListener(this);
+
+        RecyclerImageView imgView=findViewById(R.id.img_test);
+        imgView.loadImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564406714258&di=3a57d4b83d48df3194bad83a74fe81ad&imgtype=0&src=http%3A%2F%2Fimg.book118.com%2Fsr1%2FM00%2F0B%2F17%2FwKh2Al0LMjeIW64PAAEqr-JkQOQAAeGqwD9g2gAASrH405.jpg");
     }
 
     @Override
