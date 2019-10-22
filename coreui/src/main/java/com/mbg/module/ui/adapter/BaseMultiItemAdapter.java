694742c36e08efaa32e793cf9944854cfff30565
zhaozhiyang <zhaozhiyang@blued.com>
2019-10-22 17:35:54 +0800

    添加base adapter

---
+++ coreui/src/main/java/com/mbg/module/ui/adapter/BaseMultiItemAdapter.java
@@ -0,0 +1,69 @@
+package com.mbg.module.ui.adapter;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+
+import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.mbg.module.ui.adapter.model.MultiBaseItem;
+
+import java.util.List;
+
+/**
+ *
+ *created by Gap
+ * 创建默认的多Item的adapter
+ *
+ */
+public abstract class BaseMultiItemAdapter extends BaseMultiItemQuickAdapter<MultiBaseItem, BaseViewHolder> {
+    public BaseMultiItemAdapter(List<MultiBaseItem> data) {
+        super(data);
+    }
+
+    public void setTitle(@LayoutRes int layoutId){
+        addItemType(MultiBaseItem.TYPE_TITLE,layoutId);
+    }
+
+    public void setCell(@LayoutRes int layoutId){
+        addItemType(MultiBaseItem.TYPE_CELL,layoutId);
+    }
+
+    public void setLine(@LayoutRes int layoutId){
+        addItemType(MultiBaseItem.TYPE_LINE,layoutId);
+    }
+
+    public void setList(@LayoutRes int layoutId){
+        addItemType(MultiBaseItem.TYPE_LIST,layoutId);
+    }
+
+    @Override
+    protected void convert(@NonNull BaseViewHolder helper, MultiBaseItem item) {
+        switch (item.getItemType()){
+            case MultiBaseItem.TYPE_TITLE:
+                initTitle(helper,item);
+                break;
+            case MultiBaseItem.TYPE_CELL:
+                initCell(helper,item);
+                break;
+            case MultiBaseItem.TYPE_LINE:
+                initLine(helper,item);
+                break;
+            case MultiBaseItem.TYPE_LIST:
+                initList(helper,item);
+                break;
+            default:
+                initOther(helper,item);
+                break;
+        }
+    }
+
+    protected  void initTitle(BaseViewHolder helper, MultiBaseItem item){}
+
+    protected  void initCell(BaseViewHolder helper, MultiBaseItem item){}
+
+    protected  void initLine(BaseViewHolder helper, MultiBaseItem item){}
+
+    protected  void initList(BaseViewHolder helper, MultiBaseItem item){}
+
+    protected  void initOther(BaseViewHolder helper, MultiBaseItem item){}
+}
---
+++ coreui/src/main/java/com/mbg/module/ui/adapter/model/MultiBaseItem.java
@@ -0,0 +1,39 @@
+package com.mbg.module.ui.adapter.model;
+
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+
+/**
+ * created by Gap
+ * 创建默认的多item model
+ *
+ */
+public class MultiBaseItem implements MultiItemEntity {
+    public static final int TYPE_TITLE = 1;
+    public static final int TYPE_CELL = 2;
+    public static final int TYPE_LINE = 3;
+    public static final int TYPE_LIST = 4;
+
+
+    private int itemType;
+    private int spanSize;
+
+
+    public MultiBaseItem(int itemType, int spanSize){
+        this.itemType=itemType;
+        this.spanSize=spanSize;
+    }
+
+    @Override
+    public int getItemType() {
+        return itemType;
+    }
+
+    public int getSpanSize() {
+        return spanSize;
+    }
+
+    public void setTypeSpanSize(int itemType,int spanSize){
+        this.itemType=itemType;
+        this.spanSize=spanSize;
+    }
+}
