af83e401bd312eea224d841583333eca7a7830d4
zhaozhiyang <zhaozhiyang@blued.com>
2020-06-23 11:11:03 +0800

    添加ShimmerLayout和Skeleton

---
+++ coreui/src/main/java/com/mbg/module/ui/view/skeleton/ShimmerViewHolder.java
@@ -0,0 +1,28 @@
+package com.mbg.module.ui.view.skeleton;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mbg.module.ui.R;
+
+/**
+ * Created by ethanhua on 2017/7/29.
+ */
+
+public class ShimmerViewHolder extends RecyclerView.ViewHolder {
+
+    public ShimmerViewHolder(LayoutInflater inflater, ViewGroup parent, int innerViewResId) {
+        super(inflater.inflate(R.layout.view_layout_shimmer, parent, false));
+        ViewGroup layout = (ViewGroup) itemView;
+        View view = inflater.inflate(innerViewResId, layout, false);
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        if (lp != null) {
+            layout.setLayoutParams(lp);
+        }
+        layout.addView(view);
+    }
+}
