bce31beca5d37559f4ef79c714838ed91ee9557c
zhaozhiyang <zhaozhiyang@blued.com>
2021-02-23 16:13:18 +0800

    添加MotionLayout的使用的监听事件。

--- app/src/main/java/com/mbg/mbgsupport/fragment/MotionLayoutFragment.java
+++ app/src/main/java/com/mbg/mbgsupport/fragment/MotionLayoutFragment.java
@@ -2,19 +2,23 @@ package com.mbg.mbgsupport.fragment;
 
 import android.content.Context;
 import android.util.TypedValue;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import com.google.android.flexbox.FlexboxLayout;
 import com.mbg.mbgsupport.R;
 import com.mbg.module.common.util.ClickUtils;
+import com.mbg.module.common.util.LogUtils;
 import com.mbg.module.common.util.ToastUtils;
 import com.mbg.module.ui.activity.TerminalActivity;
 import com.mbg.module.ui.fragment.BaseFragment;
 
 
 public class MotionLayoutFragment extends BaseFragment implements View.OnClickListener {
-
+    MotionLayout mMotionLayout;
     public static void show(Context context) {
         TerminalActivity.show(context, MotionLayoutFragment.class, null);
     }
@@ -31,6 +35,31 @@ public class MotionLayoutFragment extends BaseFragment implements View.OnClickLi
             @Override
             public void onClick(View v) {
                 ToastUtils.show("OnClicked");
+            }
+        });
+
+        mMotionLayout=findViewById(R.id.motion_layout);
+        mMotionLayout.setTransitionListener(new MotionLayout.TransitionListener() {
+            @Override
+            public void onTransitionStarted(MotionLayout motionLayout, int startId, int i1) {
+                LogUtils.d("zzy:startId="+startId);
+                ToastUtils.show("onTransitionStarted");
+            }
+
+            @Override
+            public void onTransitionChange(MotionLayout motionLayout, int startId, int i1, float v) {
+                //LogUtils.d("zzy:startId="+startId);
+            }
+
+            @Override
+            public void onTransitionCompleted(MotionLayout motionLayout, int startId) {
+                LogUtils.d("zzy:startId="+startId);
+                ToastUtils.show("onTransitionCompleted");
+            }
+
+            @Override
+            public void onTransitionTrigger(MotionLayout motionLayout, int startId, boolean b, float v) {
+                LogUtils.d("zzy:startId="+startId);
             }
         });
     }
