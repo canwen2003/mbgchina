6857377a728995912b53cf8a2392608b91856821
zhaozhiyang <zhaozhiyang@blued.com>
2021-02-25 16:10:28 +0800

    添加Android Startup Sample

--- app/src/main/java/com/mbg/mbgsupport/app/App.java
+++ app/src/main/java/com/mbg/mbgsupport/app/App.java
@@ -9,7 +9,16 @@ import com.mbg.mbgsupport.app.startup.ImageLoaderFirstStartup;
 import com.mbg.mbgsupport.app.startup.SampleFourthStartup;
 import com.mbg.mbgsupport.app.startup.PreferencesSecondStartup;
 import com.mbg.mbgsupport.app.startup.SampleThirdStartup;
+import com.mbg.module.common.util.LogUtils;
+import com.rousetime.android_startup.StartupListener;
 import com.rousetime.android_startup.StartupManager;
+import com.rousetime.android_startup.model.CostTimesModel;
+import com.rousetime.android_startup.model.LoggerLevel;
+import com.rousetime.android_startup.model.StartupConfig;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 
 public class App extends Application {
@@ -24,12 +33,29 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        StartupConfig startupConfig = new StartupConfig.Builder()
+                .setAwaitTimeout(12000)
+                .setLoggerLevel(LoggerLevel.DEBUG)
+                .setListener(new StartupListener() {
+            @Override
+            public void onCompleted(long totalMainThreadCostTime, @NotNull List<CostTimesModel> list) {
+                LogUtils.d("totalMainThreadCostTime:"+totalMainThreadCostTime);
+                for (CostTimesModel costTimesModel :list) {
+                    LogUtils.d("getName=" + costTimesModel.getName());
+                    LogUtils.d("getCallOnMainThread=" + costTimesModel.getCallOnMainThread());
+                    LogUtils.d("getWaitOnMainThread=" + costTimesModel.getWaitOnMainThread());
+                    LogUtils.d("getStartTime=" + costTimesModel.getStartTime());
+                    LogUtils.d("getEndTime=" + costTimesModel.getEndTime()+"\r\n");
+                }
+            }
+        }).build();
         //初始化启动项
         new StartupManager.Builder()
                 .addStartup(new ImageLoaderFirstStartup())
                 .addStartup(new PreferencesSecondStartup())
                 .addStartup(new SampleThirdStartup())
                 .addStartup(new SampleFourthStartup())
+                .setConfig(startupConfig)
                 .build(this)
                 .start()
                 .await();
