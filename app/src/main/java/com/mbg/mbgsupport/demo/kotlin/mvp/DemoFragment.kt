7c0f9c34171c114cd1afb0dec49a0a288df2e2ad
zhaozhiyang <zhaozhiyang@blued.com>
2021-04-27 19:51:28 +0800

    添加Kotlin MVP 架构。

--- app/src/main/java/com/mbg/mbgsupport/demo/kotlin/mvp/DemoFragment.kt
+++ app/src/main/java/com/mbg/mbgsupport/demo/kotlin/mvp/DemoFragment.kt
@@ -5,6 +5,8 @@ import com.mbg.mbgsupport.R
 import com.mbg.mbgsupport.databinding.FragmentDemoViewBindingBinding
 import com.mbg.module.ui.mvp.kotlin.MvpFragment
 import kotlinx.android.synthetic.main.fragment_anim.*
+import kotlinx.android.synthetic.main.fragment_textbanner.*
+import kotlinx.android.synthetic.main.view_merge_demo.*
 
 
 class DemoFragment:MvpFragment<DemoPresenter,FragmentDemoViewBindingBinding>() {
@@ -21,6 +23,7 @@ class DemoFragment:MvpFragment<DemoPresenter,FragmentDemoViewBindingBinding>() {
         super.onInitView(savedInstanceState)
         mViewBinding?.apply {
             btn_pop_out.text="onInitView"
+            tv_mr_view1.text="Merge child";
         }
     }
 
